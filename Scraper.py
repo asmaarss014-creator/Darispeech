362bcc357e99844f83da32bdcedfebd3dec6b39d
asmaarss014-creator <asmaarss014@gmail.com>
2026-07-25 15:36:11 +0430

    Fix indentation and clean up comments in Scraper.py

--- Scraper.py
+++ Scraper.py
@@ -5,7 +5,7 @@ def download_data(url):
     print(f"Downloading {url}...")
     ydl_opts = {
         'format': 'bestaudio/best',
-        'outtmpl': 'audio_%(id)s.%(ext)s', # Saves the audio file in the repo
+        'outtmpl': 'audio_%(id)s.%(ext)s',
         'writesubtitles': True,
         'writeautomaticsub': True,
         'subtitleslangs': ['fa', 'fa-AF', 'prs'],
@@ -16,7 +16,6 @@ def download_data(url):
     print("Done downloading!")
 
 if __name__ == "__main__":
-    # GitHub Actions will pass the URL as an environment variable
     video_url = os.environ.get('VIDEO_URL')
     if video_url:
         download_data(video_url)
